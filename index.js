f924b8f84a91f235d6d30c2673f7ea3bf27f0a97
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 19:44:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(currentLineNo,custname){
-  console.log(currentLineNo)
+  console.log(currentLineNo+" Line number")
   currentLineNo.push(custname)
-  console.log(currentLineNo)
+  console.log(currentLineNo + "new line no")
   console.log(`Welcome, ${custname}.You are number ${currentLineNo.length} in line`)
   return `Welcome, ${custname}.You are number ${currentLineNo.length} in line`
   
