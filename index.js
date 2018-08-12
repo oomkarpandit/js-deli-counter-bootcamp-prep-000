9dcfa8f29396dc94e0a368ffe7c1be9bec3362da
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 19:36:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function takeANumber(currentLineNo,custname){
-
-  katzDeliLine[currentLineNo.length]=custname
-
-return `Welcome, ${katzDeliLine[currentLineNo]. You are number }`
+  katzDeliLine.push(custname)
   
   
 }
